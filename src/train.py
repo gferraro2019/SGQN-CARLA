f97e1c1d3d89d8262218b64823a408a5fcb5424f
Giuseppe <gferraro2016@gmail.com>
2023-05-10 09:55:16 +0200

    add some comment in train.py

--- src/train.py
+++ src/train.py
@@ -12,7 +12,9 @@ from logger import Logger
 from video import VideoRecorder
 
 
-def evaluate(env, agent,algorithm, video, num_episodes, L, step, test_env=False, eval_mode=None):
+def evaluate(
+    env, agent, algorithm, video, num_episodes, L, step, test_env=False, eval_mode=None
+):
     episode_rewards = []
     for i in range(num_episodes):
         obs = env.reset()
@@ -31,7 +33,7 @@ def evaluate(env, agent,algorithm, video, num_episodes, L, step, test_env=False,
                 video.record(env)
                 episode_reward += reward
                 # log in tensorboard 15th step
-                if algorithm == 'sgsac':
+                if algorithm == "sgsac":
                     if i == 0 and episode_step in [15, 16, 17, 18] and step > 0:
                         _obs = agent._obs_to_input(obs)
                         torch_obs.append(_obs)
@@ -66,6 +68,7 @@ def main(args):
     home = os.environ["HOME"]
     os.environ["MJKEY_PATH"] = f"{home}/.mujoco/mujoco200_linux/bin/mjkey.txt"
     os.environ["MUJOCO_GL"] = "egl"
+
     # Set seed
     utils.set_seed_everywhere(args.seed)
 
@@ -140,12 +143,16 @@ def main(args):
 
     # Prepare agent
     assert torch.cuda.is_available(), "must have cuda enabled"
+
+    # Create replay buffer
     replay_buffer = utils.ReplayBuffer(
         obs_shape=env.observation_space.shape,
         action_shape=env.action_space.shape,
         capacity=args.train_steps,
         batch_size=args.batch_size,
     )
+
+    # Define observation
     cropped_obs_shape = (
         3 * args.frame_stack,
         args.image_crop_size,
@@ -153,14 +160,22 @@ def main(args):
     )
     print("Observations:", env.observation_space.shape)
     print("Cropped observations:", cropped_obs_shape)
+
+    # create agent
     agent = make_agent(
         obs_shape=cropped_obs_shape, action_shape=env.action_space.shape, args=args
     )
 
+    # Initialize variables
     start_step, episode, episode_reward, done = 0, 0, 0, True
+
+    # Define logger
     L = Logger(work_dir)
+
+    # Start training
     start_time = time.time()
     for step in range(start_step, args.train_steps + 1):
+        # EVALUATE:
         if done:
             if step > start_step:
                 L.log("train/duration", time.time() - start_time, step)
@@ -171,7 +186,7 @@ def main(args):
             if step % args.eval_freq == 0:
                 print("Evaluating:", work_dir)
                 L.log("eval/episode", episode, step)
-                evaluate(env, agent,args.algorithm, video, args.eval_episodes, L, step)
+                evaluate(env, agent, args.algorithm, video, args.eval_episodes, L, step)
                 if test_envs is not None:
                     for test_env, test_env_mode in zip(test_envs, test_envs_mode):
                         evaluate(
@@ -190,27 +205,32 @@ def main(args):
             # Save agent periodically
             if step > start_step and step % args.save_freq == 0:
                 torch.save(
-                    agent.actor.state_dict(), os.path.join(model_dir, f"actor_{step}.pt")
+                    agent.actor.state_dict(),
+                    os.path.join(model_dir, f"actor_{step}.pt"),
                 )
                 torch.save(
-                    agent.critic.state_dict(), os.path.join(model_dir, f"critic_{step}.pt")
+                    agent.critic.state_dict(),
+                    os.path.join(model_dir, f"critic_{step}.pt"),
                 )
                 if args.algorithm == "sgsac":
                     torch.save(
-                        agent.attribution_predictor.state_dict(), os.path.join(model_dir, f"attrib_predictor_{step}.pt")
+                        agent.attribution_predictor.state_dict(),
+                        os.path.join(model_dir, f"attrib_predictor_{step}.pt"),
                     )
-
 
             L.log("train/episode_reward", episode_reward, step)
 
+            # Reset environment
             obs = env.reset()
             done = False
             episode_reward = 0
             episode_step = 0
+
             episode += 1
 
             L.log("train/episode", episode, step)
 
+        # TRAIN:
         # Sample action for data collection
         if step < args.init_steps:
             action = env.action_space.sample()
@@ -227,10 +247,12 @@ def main(args):
         # Take step
         next_obs, reward, done, _ = env.step(action)
         done_bool = 0 if episode_step + 1 == env._max_episode_steps else float(done)
+
+        # Update replay buffer
         replay_buffer.add(obs, action, reward, next_obs, done_bool)
+
         episode_reward += reward
         obs = next_obs
-
         episode_step += 1
 
     print("Completed training for", work_dir)
