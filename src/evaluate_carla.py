0d84d7556609e8dfcf6255bd2275567fe32b9c9d
Giuseppe <gferraro2016@gmail.com>
2023-10-23 15:41:45 +0200

    edit path

--- src/evaluate_carla.py
+++ src/evaluate_carla.py
@@ -47,7 +47,7 @@ def main(args):
         False,
         "Custom",  # "All",
         max_episode_steps,
-        lower_limit_cumulative_reward=args.lower_limit_cumulative_reward,
+        lower_limit_return_=args.lower_limit_return_,
     )
     env = FrameStack_carla(env, args.frame_stack)
 
@@ -59,16 +59,16 @@ def main(args):
     # Create the agent
     agent = make_agent(obs_shape=shp, action_shape=env.action_space.shape, args=args)
 
-    folder = 10143
+    folder = 10164
 
     # Load existing actor and critic
     episodes = [str(i) for i in range(100, 2600, 100)]
     for e in episodes:
         actor_state_dict = torch.load(
-            f"/home/dcas/g.ferraro/gitRepos/SGQN-CARLA/src/logs/carla_drive/sac/{folder}/model/actor_{e}.pt"
+            f"/home/dcas/g.ferraro/gitRepos/SGQN-CARLA/logs/carla_drive/sac/{folder}/model/actor_{e}.pt"
         )
         critic_state_dict = torch.load(
-            f"/home/dcas/g.ferraro/gitRepos/SGQN-CARLA/src/logs/carla_drive/sac/{folder}/model/critic_{e}.pt"
+            f"/home/dcas/g.ferraro/gitRepos/SGQN-CARLA/logs/carla_drive/sac/{folder}/model/critic_{e}.pt"
         )
 
         print(f"Evaluating actor and critic realted to episode {e}")
